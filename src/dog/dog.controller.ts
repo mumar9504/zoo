d24df0293e6972be4c57b9b480b90878115a466f
mumar9504 <minu0709ms@gmail.com>
2024-08-13 20:47:55 +0900

    feat: create post api request

--- src/dog/dog.controller.ts
+++ src/dog/dog.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query, Req, Res } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { DogService } from './dog.service';
 import { query } from 'express';
 
@@ -12,12 +12,15 @@ export class DogController {
   }
 
   @Get('greet/:id')
-  public introduce(
-    @Param() params: any,
-    @Query() query: any,
-  ): string {
+  public introduce(@Param() params: any, @Query() query: any): string {
     console.log('params:', params);
     console.log('query:', query);
     return this.dogService.introduce();
+  }
+
+  @Post('edit')
+  public modifyDetail(@Body() body: any): string {
+    console.log('body:', body);
+    return this.dogService.modifyDetail();
   }
 }
